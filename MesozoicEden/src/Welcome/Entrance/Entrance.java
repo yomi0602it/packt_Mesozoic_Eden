91dc6c966fafe875f983c82a422b53d053ebb7e0
le_clone <yomi0602it@gmail.com>
2025-06-25 11:41:45 -0400

    Added README. Created Java project. Added first package

---
+++ MesozoicEden/src/Welcome/Entrance/Entrance.java
@@ -0,0 +1,11 @@
+package packt-Mesozoic-Eden.MesozoicEden.src.Welcome.Entrance;
+
+public class Entrance {
+public static void main(String[] args) {
+        System.out.println("Welcome to Mesozoic Eden");
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+        System.out.println("We open at " + openingHours + " and close at " + closingHours + ".");
+        System.out.println("Mesozoic Eden is safe and secure.");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # packt-Mesozoic-Eden
 Java projects from 'Learn Java with Projects' by Dr. Sean Kennedy and Maaike van Putten
+  
+Projects are dinosaur themed and include various software development tasks as an employee of Mesozoic Eden, from coding software for feeding schedules to the app that handles emergency alerts. Goal of Mesozoic Eden is to ensure park operations run smoothly and above all, creating an unforgettable experience for visitors!
